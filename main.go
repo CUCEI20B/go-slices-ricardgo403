a85acf1ee6783df0283b5ec2d3d266757ebf65c2
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-10-12 21:30:01 -0500

    fix(main): Capture the size of a slice and iterate and append values to it. Then iterate over it and get the sum of all the values.

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var tamanio uint
+	fmt.Scan(&tamanio)
+	var mySlice = make([]uint, 0, tamanio)
+	var sumatoria uint
+	for i := 0; i < int(tamanio); i++ {
+		var captured uint
+		if _, err := fmt.Scan(&captured); err != nil {
+			fmt.Println(err)
+			return
+		}
+		mySlice = append(mySlice, captured)
+	}
+	for _, v := range mySlice {
+		sumatoria += v
+	}
+	fmt.Println(sumatoria)
 }
